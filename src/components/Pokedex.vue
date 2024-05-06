ca08753bbed21e272a701668b3309a70478723ac
PedroLP7 <113777475+PedroLP7@users.noreply.github.com>
2024-05-06 15:52:49 +0200

    boton catched

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -18,14 +18,14 @@
         <div class="row mt-3">
           
             
-            <div class="card col-3 m-5 align-content-center " v-for="pokemon in filteredPokemons" :key="pokemon.id">
+            <div class="card col-3 m-5   align-content-center " v-for="pokemon in filteredPokemons" :key="pokemon.id">
                 
 
                 <svg  @click="getLike(pokemon)"  class="like mt-3"xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512">
 		<path fill="#e0cccc" d="m47.6 300.4l180.7 168.7c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9l180.7-168.7c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141c-45.6-7.6-92 7.3-124.6 39.9l-12 12l-12-12c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5" />
 	</svg>
     <input type="checkbox" class="btn-check teamcheck" :id="'btn-check-outlined-' + pokemon.id" v-model="pokemon.caught" @change="toggleCaught(pokemon)">
-            <label class="btn btn-outline-primary team" :for="'btn-check-outlined-' + pokemon.id">{{ pokemon.caught ? 'RELEASE' : 'CATCH' }}</label><br>
+            <label class="btn btn-outline-primary team" :for="'btn-check-outlined-' + pokemon.id">{{ pokemon.caught ? 'CATCHED' : 'CATCH' }}</label><br>
    
    
               <img :src="pokemon.sprites.front_default" class="card-img-top" alt="...">
@@ -320,7 +320,7 @@ export default {
         align-content: center;
         top: 15px;
         right: 15px;
-        width: 52px;
+        width: 65px;
         height: 40px;
 
         font-size: 12px;
